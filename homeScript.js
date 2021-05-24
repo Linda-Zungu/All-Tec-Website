5cea3e4fbd6b8978913526566fe76b8e269eeab6
Linda Zungu <linda.zungu@icloud.com>
2021-05-25 01:39:03 +0200

    Created image animations.

--- homeScript.js
+++ homeScript.js
@@ -2,13 +2,21 @@ function checkScrollPosition(){
     let scroll = this.scrollY;
     console.log(scroll);
 
-    let comSciPic = document.getElementById("comsci");
     document.getElementById("comscipicText").style.opacity = ""+scroll/300;
     
-    // if(scroll < 150){
-    //     comSciPic.style.filter = "blur("+scroll/25+"px)";
-    // }
-    
+    if(scroll > 450){
+        document.getElementById("htmlImage").style.animation = "showUp 3.5s ease-in-out";
+        document.getElementById("cssImage").style.animation = "showUp 2.0s ease-in-out";
+        document.getElementById("jsImage").style.animation = "showUp 3.5s ease-in-out";
+    }
+    if(scroll > 850){
+        document.getElementById("stackImage").style.animation = "showUp 3.5s ease-in-out";
+        document.getElementById("pythonImage").style.animation = "showUp 2.0s ease-in-out";
+        document.getElementById("javaImage").style.animation = "showUp 3.5s ease-in-out";
+    }
+    if(scroll > 1250){
+        document.getElementById("swiftImage").style.animation = "showUp 2.0s ease-in-out";
+    }
 }
 
 window.addEventListener("scroll", function() {checkScrollPosition()})
