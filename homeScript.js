d7335f7f6e4ab5fc2186cd2d37ab16beec6eb4de
Linda Zungu <linda.zungu@icloud.com>
2021-05-26 01:23:42 +0200

    The section of courses from the nav bar can animate and becomes visible when hovering over the courses tab on the nav bar

--- homeScript.js
+++ homeScript.js
@@ -19,4 +19,24 @@ function checkScrollPosition(){
     }
 }
 
-window.addEventListener("scroll", function() {checkScrollPosition()})
+window.addEventListener("scroll", function() {checkScrollPosition()});
+
+let coursesTab = document.getElementById("coursesID");
+let courseLinkNav = document.getElementById("courseIconID");
+
+coursesTab.addEventListener("mouseover", function (){
+    courseLinkNav.style.animation = "moveIconLinks 0.5s ease-in-out";
+    courseLinkNav.style.transform = "translateY(44px)"
+    courseLinkNav.style.opacity = "1"
+})
+
+courseLinkNav.addEventListener("mouseover", function (){
+    courseLinkNav.style.transform = "translateY(44px)"
+    courseLinkNav.style.opacity = "1"
+})
+
+courseLinkNav.addEventListener("mouseout", function (){
+    courseLinkNav.style.transform = "translateY(0px)"
+    courseLinkNav.style.opacity = "0"
+    courseLinkNav.style.transition = "all 0.3s ease-in-out"
+})
