84090e258de6548c0c11e786aa5eddbca078997a
Linda Zungu <linda.zungu@icloud.com>
2021-06-01 21:44:27 +0200

    Added a css course page

---
+++ cssCourseScript.js
@@ -0,0 +1,25 @@
+let coursesTab = document.getElementById("coursesID");
+let courseLinkNav = document.getElementById("courseIconID");
+
+coursesTab.addEventListener("mouseover", function (){
+    courseLinkNav.style.animation = "moveIconLinks 0.5s ease-in-out";
+    courseLinkNav.style.transform = "translateY(44px)"
+    courseLinkNav.style.opacity = "1"
+
+    coursesTab.addEventListener("mouseout", function (){
+        courseLinkNav.style.transform = "translateY(0px)"
+        courseLinkNav.style.opacity = "0"
+        courseLinkNav.style.transition = "all 0.3s ease-in-out"
+    })
+})
+
+courseLinkNav.addEventListener("mouseover", function (){
+    courseLinkNav.style.transform = "translateY(44px)"
+    courseLinkNav.style.opacity = "1"
+})
+
+courseLinkNav.addEventListener("mouseout", function (){
+    courseLinkNav.style.transform = "translateY(0px)"
+    courseLinkNav.style.opacity = "0"
+    courseLinkNav.style.transition = "all 0.3s ease-in-out"
+})
